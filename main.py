515134ad012c933361fb05182b1ed3524bc7328e
Sanjeev <35099154+rssanjeev@users.noreply.github.com>
2024-03-05 06:47:29 -0500

    main.py updated with basic functions.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import time, requests, pandas as pd
 
 def get_data():
-    url = "https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum&timezone=America%2FDenver"
-    response = requests.get(url)
-    return pd.DataFrame(response.json())
+    current_weather_url, weather_code_url = get_url()
+    response = requests.get(current_weather_url)
+    response2 = requests.get(weather_code_url)
+    return pd.DataFrame(response.json()), pd.DataFrame(response2.json())
+
+def get_url():
+    current_weather_url = "https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&current_weather=true&timezone=America%2FNew_York"
+    weather_code_url = "https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&daily=weathercode"
+    return current_weather_url, weather_code_url
 
 def process_data():
-    data = get_data()
+    current_weather_data, weather_code_data = get_data()
     #data = data.drop(['latitude', 'longitude', 'generationtime_ms', 'utc_offset_seconds','timezone', 'timezone_abbreviation', 'elevation', 'hourly_units'], axis=1)
     #data = data.pivot(columns='hourly')
-    print(data)
-    #data.to_csv('Hourly.csv')
+    print(current_weather_data)
+    print(weather_code_data)
+    current_weather_data.to_csv('Current.csv')
+    weather_code_data.to_csv('WeatherCode`.csv')
 
 def send_data():
     pass
